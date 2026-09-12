ecd80fd0161e7d3bdfa03734b87d5e6bb09be3cc
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-12 13:37:03 +0700

    update: new nodes

--- main.py
+++ main.py
@@ -25,13 +25,19 @@ def delete_gm(head, target_gm):
 
 
 node1 = GuildMembers("Angelica ven Ashera")
-node2 = GuildMembers("Voile de Lavilliant")
+node2 = GuildMembers("Serafina de Lavilliant")
 node3 = GuildMembers("Emilia")
 node4 = GuildMembers("Sylvie Leywin")
+node5 = GuildMembers("Audrey Hall")
+node6 = GuildMembers("Nephis")
+node7 = GuildMembers("Alyssa Maclius")
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
 
 print_gm(node1)
 new_gm = delete_gm(node1, "Emilia")
